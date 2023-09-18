a1265c36f8b414b6d4fe2476c36a5bc44e690da1
Antony Lester <ant.l@post.com>
2023-09-18 07:52:47 +0100

    Fix For Issue #70

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 echo "Installing libraries"
-sudo apt install python3-pip libhidapi-libusb0 libxcb-xinerama0
+sudo apt install python3-pip libhidapi-libusb0 libxcb-xinerama0 '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
 
 echo "Adding udev rules and reloading"
 sudo tee /etc/udev/rules.d/70-streamdeck.rules << EOF
